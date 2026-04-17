79687e4aedc2c24e3a371f71cecac57106260bb4
Lokman Sarkar <lokmansarker13@gmail.com>
2026-04-17 20:08:31 +0600

    About and Resume Updated

--- src/layouts/RootLayout/Header/NavBar.tsx
+++ src/layouts/RootLayout/Header/NavBar.tsx
@@ -2,13 +2,13 @@ import styled from "@emotion/styled"
 import Link from "next/link"
 
 const NavBar: React.FC = () => {
-  const links = [{ id: 1, name: "About", to: "/about" }]
+  const links = [{ id: 1, name: "About", to: "https://lokman-sarkar.vercel.app" }, {id: 2, name: "Resume", to: "https://www.canva.com/design/DAG62ogKVmw/qSlvIBj8MmnFTyTbnv67hw/edit"}]
   return (
     <StyledWrapper className="">
       <ul>
         {links.map((link) => (
           <li key={link.id}>
-            <Link href={link.to}>{link.name}</Link>
+            <Link href={link.to} target="_blank" rel="noopener noreferrer">{link.name} <span className=""></span></Link>
           </li>
         ))}
       </ul>
